fcc57db0fa4ac186bcf0ff1efcafcbc2bd302fd2
Valdir Silva <valdipiresba@gmail.com>
2024-01-27 20:58:04 -0300

    feat: project hr-payroll created

---
+++ payrol/src/main/java/com/hrpayroll/payrol/PayrolApplication.java
@@ -0,0 +1,13 @@
+package com.hrpayroll.payrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrolApplication.class, args);
+	}
+
+}
---
+++ payrol/src/test/java/com/hrpayroll/payrol/PayrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrpayroll.payrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
