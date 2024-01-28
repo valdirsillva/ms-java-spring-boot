0406406ae5e6efc8e7d7aa1cfa78104d55a000bd
Valdir Silva <valdipiresba@gmail.com>
2024-01-27 21:02:55 -0300

    feat: project hr-payroll created

--- payrol/src/main/java/com/hrpayroll/payrol/PayrolApplication.java
+++ payrol/src/main/java/com/hrpayroll/payrol/PayrolApplication.java
@@ -1,13 +1,13 @@
-package com.hrworker.worker;
+package com.hrpayroll.payrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WorkerApplication {
+public class PayrolApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorkerApplication.class, args);
+		SpringApplication.run(PayrolApplication.class, args);
 	}
 
 }
--- worker/src/main/java/com/hrworker/worker/repositories/WorkerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hrworker.worker.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hrworker.worker.entities.Worker;
-
-public interface WorkerRepository extends JpaRepository<Worker, Long> {
-
-}
