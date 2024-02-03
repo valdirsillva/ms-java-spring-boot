6f49bae4b891a7b6d9b8f326fdb5702076bd4649
Valdir Silva <valdipiresba@gmail.com>
2024-02-03 18:20:07 -0300

    feat: configure api-gateway-zuul

--- api-gateway-zuul/src/main/java/com/valdirsillva/apigatewayzuul/ApiGatewayZuulApplication.java
+++ api-gateway-zuul/src/main/java/com/valdirsillva/apigatewayzuul/ApiGatewayZuulApplication.java
@@ -2,7 +2,11 @@ package com.valdirsillva.apigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class ApiGatewayZuulApplication {
 
