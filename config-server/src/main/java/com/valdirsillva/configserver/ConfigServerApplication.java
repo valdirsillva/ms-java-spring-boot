8490f342d2aeb23b6d91f6d8d8a761148b8a9f73
Valdir Silva <valdipiresba@gmail.com>
2024-02-05 13:37:08 -0300

    chore: private Git repository

--- config-server/src/main/java/com/valdirsillva/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/valdirsillva/configserver/ConfigServerApplication.java
@@ -1,15 +1,26 @@
 package com.valdirsillva.configserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigServerApplication {
+public class ConfigServerApplication implements CommandLineRunner {
+
+	@Value("${spring.cloud.config.server.git.username}")
+	private String username;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		// System.out.println("USER = " + username);
 	}
 
 }
